64a449d0da9281e72f9c28681d33265dabae9c25
MDB0102 <94869616+MDB0102@users.noreply.github.com>
2026-06-12 16:06:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,34 @@
+
 const CACHE_NAME = '6quiprend-v1';
 
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './CSS/style.css',
+  './icon-192.png',
+  './icon-512.png',
+  './css/styles.css',
   './js/app.js'
 ];
 
-// Installation : mise en cache de tous les fichiers
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-// Activation : suppression des anciens caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+    caches.keys().then((keyList) =>
+      Promise.all(keyList.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-// Interception des requêtes : cache d'abord, réseau ensuite
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
